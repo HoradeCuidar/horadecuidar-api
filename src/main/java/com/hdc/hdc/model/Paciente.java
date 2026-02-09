b476b546d7bd0da1e1ab47b81e101e4718e7bed2
Miqueias Bento <miqueiasbto32@alu.ufc.br>
2026-02-09 14:32:10 -0300

    chore: adicionando atributos de doenca nas entidades

--- src/main/java/com/hdc/hdc/model/Paciente.java
+++ src/main/java/com/hdc/hdc/model/Paciente.java
@@ -3,36 +3,44 @@ package com.hdc.hdc.model;
 import com.hdc.hdc.model.enums.Genero;
 import com.hdc.hdc.model.enums.Role;
 import com.hdc.hdc.model.enums.Status;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @DiscriminatorValue("PACIENTE")
 @Entity
-public class Paciente extends Usuario{
+public class Paciente extends Usuario {
+
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
+    @JoinTable(name = "paciente_doencas", joinColumns = @JoinColumn(name = "paciente_id"), inverseJoinColumns = @JoinColumn(name = "doenca_id"))
+    private List<Doenca> doencas;
+
+    @Column(columnDefinition = "text", name = "observacoes")
+    private String observacoes;
 
     public Paciente(Integer id,
-                    String nome,
-                    String username,
-                    String senha,
-                    LocalDate dataDeNascimento,
-                    Role role,
-                    Status status,
-                    String telefone,
-                    String rua,
-                    String bairro,
-                    String estado,
-                    String cidade,
-                    String numeroDaCasa,
-                    Genero genero,
-                    String email){
-        super(id, nome, username, senha, dataDeNascimento, role, status, telefone, rua, bairro, estado, cidade, numeroDaCasa, genero, email);
+            String nome,
+            String username,
+            String senha,
+            LocalDate dataDeNascimento,
+            Role role,
+            Status status,
+            String telefone,
+            String rua,
+            String bairro,
+            String estado,
+            String cidade,
+            String numeroDaCasa,
+            Genero genero,
+            String email) {
+        super(id, nome, username, senha, dataDeNascimento, role, status, telefone, rua, bairro, estado, cidade,
+                numeroDaCasa, genero, email);
     }
 }
